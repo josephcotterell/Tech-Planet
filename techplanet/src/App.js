8be6ae00972bde78fc655ca28f49cfc10f20cc30
joseph <josephcotterell@hotmail.co.uk>
2023-04-24 21:08:16 +0100

    fixed error

--- techplanet/src/App.js
+++ techplanet/src/App.js
@@ -1,24 +1,17 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Navbar } from "./components/navbar";
-import { Shop } from "./pages/shop/shop";
-import { Contact } from "./pages/contact";
-import { Cart } from "./pages/cart/cart";
-import { ShopContextProvider } from "./context/shop-context";
-
 function App() {
   return (
     <div className="App">
-      <ShopContextProvider>
-        <Router>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Shop />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/cart" element={<Cart />} />
-          </Routes>
-        </Router>
-      </ShopContextProvider>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" />
+          <Route path="/" />
+          <Route path="/cart" />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- techplanet/src/components/navbar.jsx
+++ techplanet/src/components/navbar.jsx
@@ -8,8 +8,8 @@ export const Navbar = () => {
     <div className="navbar">
       <div className="links">
         <Link to="/"> Shop </Link>
-        <Link to="/contact"> Contact </Link>
-        <Link to="/cart">
+        <Link to="/profile"> Profile  </Link>
+        <Link to="/cart"> 
           <ShoppingCart size={32} />
         </Link>
       </div>
