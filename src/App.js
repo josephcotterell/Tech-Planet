ed6382439341745a3049813df38c6ab52a342492
joseph <josephcotterell@hotmail.co.uk>
2023-05-02 10:08:51 +0100

    added register.jsx

--- src/App.js
+++ src/App.js
@@ -5,27 +5,36 @@ import { Shop } from "./pages/shop/shop";
 import { Contact } from "../src/contact";
 import { Cart } from "./pages/cart/cart";
 import { ShopContextProvider } from "./pages/shop-context";
-import LoginButton from "./components/LoginButton";
-import LogoutButton from "./components/LogoutButton";
+import Login from "../src/components/Login";
+import Logout from "../src/components/Logout";
+import { Register } from "./components/Register";
+import React, { useState } from "react";
 import Profile from "./components/Profile";
 function App() {
-  return (
-    <div className="App">
-      <ShopContextProvider>
-        <Router>
-          <Navbar />
-          <LoginButton />
-          <LogoutButton />
-          <Profile />
-          <Routes>
-            <Route path="/" element={<Shop />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/cart" element={<Cart />} />
-          </Routes>
-        </Router>
-      </ShopContextProvider>
-    </div>
-  );
+  const [currentForm, setCurrentForm] = useState("login");
+  const toggleForm = (formName) => {
+    setCurrentForm(formName);
+  };
+  return;
+  <div className="App">
+    <ShopContextProvider>
+      <Router>
+        <Navbar />
+        {currentForm === "login" ? (
+          <Login onFormSwitch={toggleForm} />
+        ) : (
+          <Register onFormSwitch={toggleForm} />
+        )}
+        <Logout />
+        <Profile />
+        <Routes>
+          <Route path="/" element={<Shop />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </Router>
+    </ShopContextProvider>
+  </div>;
 }
 
 export default App;
--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -1,9 +1,9 @@
 import { useAuth0 } from "@auth0/auth0-react";
 
-const LogoutButton = () => {
+const Logout = () => {
   const { logout, isAuthenticated } = useAuth0();
 
   return isAuthenticated && <button onClick={() => logout()}>Sign Out</button>;
 };
 
-export default LogoutButton;
+export default Logout;
