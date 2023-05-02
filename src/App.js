b8083e223fa5de7f1bcc2597bc38043794aa0dfd
joseph <josephcotterell@hotmail.co.uk>
2023-05-02 09:25:21 +0100

    added Auth0

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { Cart } from "./pages/cart/cart";
 import { ShopContextProvider } from "./pages/shop-context";
 import LoginButton from "./components/LoginButton";
 import LogoutButton from "./components/LogoutButton";
-
+import Profile from "./components/Profile";
 function App() {
   return (
     <div className="App">
