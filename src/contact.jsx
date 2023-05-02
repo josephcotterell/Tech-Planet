e0678dcc3190652bb5f88c1b47f0636b21473e6e
joseph <josephcotterell@hotmail.co.uk>
2023-05-02 18:34:47 +0100

    fixed typo

--- src/contact.jsx
+++ src/contact.jsx
@@ -3,9 +3,9 @@ import React, { useState } from 'react'
 export const Contact = () => {
     return (
         <div id="contact-form">
-            <input type="text" placeholder="Your Name" />
-            <input type="email" placeholder="Your email address" />
-            <textarea placeholder="Your message"></textarea>
+            <input type="Text" placeholder="Your Name" />
+            <input type="Email" placeholder="Your Email Address" />
+            <textarea placeholder="Your Message"></textarea>
             <button>Send Message</button>
             <span>Thank you for your message, we will be in touch soon!</span>
         </div>
