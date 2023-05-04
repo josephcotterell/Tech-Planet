b2b9abf3172e10d7e27511847af628ecc9d0babe
Sakariya95 <niyyex@live.co.uk>
2023-05-04 20:51:56 +0100

    changes

--- server.js
+++ server.js
@@ -1,66 +1,24 @@
-const express = require('express');
-const Product = require('./models/Product');
-const User = require('./models/User');
-const Order = require('./models/Order');
-const graphqlHTTP = require('express-graphql');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(passport.initialize());
+app.use(express.static("public"));
 
-// Connect to database
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
-mongoose.connection.once('open', () =>
-  console.log('Connected to MongoDB database')
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost:27017",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
 );
 
-// GraphQL schema and resolvers
-const schema = require('./schema');
-const rootValue = require('./resolvers');
+//Use this to log  mongo queries being executed
+mongoose.set("debug", true);
 
-// JWT authentication strategy
-const { JWT_SECRET } = process.env;
-const JwtStrategy = require('passport-jwt').Strategy,
-  ExtractJwt = require('passport-jwt').ExtractJwt;
-const opts = {};
-opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-opts.secretOrKey = JWT_SECRET;
+app.use(require("./routes"));
 
-passport.use(
-  new JwtStrategy(opts, async (jwt_payload, done) => {
-    try {
-      const user = await User.findById(jwt_payload.id);
-      if (user) {
-        return done(null, user);
-      } else {
-        return done(null, false);
-      }
-    } catch (error) {
-      return done(error, false);
-    }
-  })
-);
-
-// GraphQL endpoint
-app.use(
-  '/graphql',
-  passport.authenticate('jwt', { session: false }),
-  graphqlHTTP({
-    schema,
-    rootValue,
-    graphiql: true,
-  })
-);
-
-// Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Connected on localhost: ${PORT}`));
